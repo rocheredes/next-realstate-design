5d0c50560e73c893f91b706caa31d00a0e96c6cf
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-10-10 23:23:57 -0400

    create slider for main section

---
+++ components/sliders/MainSlider.tsx
@@ -0,0 +1,100 @@
+'use client'
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './style.css'
+import NavigationArrows from './NavigationArrows';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
+
+
+
+const MainSlider = () => {
+
+    const pagination = {
+        el: '.pagination',
+        clickable: true,
+        renderBullet: function (index: number, className: string) {
+            return (
+                '<div class="' + className + ' !flex-1 !h-1 !rounded-lg cursor-pointer">' + '' + '</div>'
+            )
+        },
+    };
+
+    const navigation = {
+        nextEl: '.swipper-arrows-next',
+        prevEl: '.swipper-arrows-prev'
+    }
+
+    return (
+        <>
+            <Swiper
+                navigation={navigation}
+                pagination={pagination}
+                modules={[Pagination, Navigation]}
+            >
+                <SwiperSlide>
+                    <div className='relative h-48 w-full rounded-3xl overflow-hidden'>
+                        <Image
+                            fill
+                            src='/images/c0db865f789961bb3e28500f19c35c4e.jpg'
+                            alt='House Image'
+                            className='object-cover'
+                        />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='relative h-48 w-full rounded-3xl overflow-hidden'>
+                        <Image
+                            fill
+                            src='/images/8363fc4f62fd9a46cc28eb202059adff.jpg'
+                            alt='House Image'
+                            className='object-cover'
+                        />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='relative h-48 w-full rounded-3xl overflow-hidden'>
+                        <Image
+                            fill
+                            src='/images/8363fc4f62fd9a46cc28eb202059adff.jpg'
+                            alt='House Image'
+                            className='object-cover'
+                        />
+                    </div>
+
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='relative h-48 w-full rounded-3xl overflow-hidden'>
+                        <Image
+                            fill
+                            src='/images/3c1780f6158b1d62f2036152ec7a594f.jpg'
+                            alt='House Image'
+                            className='object-cover'
+                        />
+                    </div>
+                </SwiperSlide>
+
+                <div className="flex justify-start space-x-3 mt-3 mx-4 pagination" />
+                <div className="swipper-arrows-next next">
+                    <div className="main-swipper-arrow">
+                        <ChevronRightIcon className='w-6 h-6 text-light-1' />
+                    </div>
+                </div>
+
+                <div className="swipper-arrows-prev prev">
+                    <div className="main-swipper-arrow">
+                        <ChevronLeftIcon className='w-6 h-6 text-light-1' />
+                    </div>
+                </div>
+
+
+            </Swiper>
+
+        </>
+    )
+}
+
+export default MainSlider
