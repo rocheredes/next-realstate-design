227d3ded3adb1dca030e0b80707b7110ebc03ea6
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-10-15 18:39:08 -0400

    constants to work

---
+++ constant/index.js
@@ -0,0 +1,17 @@
+export const companyImages = [
+    {url:'/images/google.png', alt:'google'},
+    {url:'/images/amazon.png', alt:'amazon'},
+    {url:'/images/logitech.png', alt:'logitech'},
+    {url:'/images/spotify.png', alt:'spotify'},
+    {url:'/images/samsung.png', alt:'samsung'},
+    {url:'/images/netflix.png', alt:'netflix'},
+]
+
+export const userGroupsImages = [
+    {url:'/images/users/user1.jpg', alt:'user1'},
+    {url:'/images/users/user2.jpg', alt:'user2'},
+    {url:'/images/users/user3.jpg', alt:'user3'},
+    {url:'/images/users/user4.jpg', alt:'user4'},
+    {url:'/images/users/user5.jpg', alt:'user5'},
+    {url:'/images/users/user5.jpg', alt:'user5'},
+]
