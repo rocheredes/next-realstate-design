baf7bc75a41fa6f4fdf35b7c38ce4d7c66d7ae38
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-10-17 19:07:52 -0400

    create categories, properties constants

--- constant/index.js
+++ constant/index.js
@@ -1,17 +1,73 @@
 export const companyImages = [
-    {url:'/images/google02.png', alt:'google'},
-    {url:'/images/amazon02.png', alt:'amazon'},
-    {url:'/images/logitech02.png', alt:'logitech'},
-    {url:'/images/spotify02.png', alt:'spotify'},
-    {url:'/images/samsung02.png', alt:'samsung'},
-    {url:'/images/netflix02.png', alt:'netflix'},
+    { url: '/images/google02.png', alt: 'google' },
+    { url: '/images/amazon02.png', alt: 'amazon' },
+    { url: '/images/logitech02.png', alt: 'logitech' },
+    { url: '/images/spotify02.png', alt: 'spotify' },
+    { url: '/images/samsung02.png', alt: 'samsung' },
+    { url: '/images/netflix02.png', alt: 'netflix' },
 ]
 
 export const userGroupsImages = [
-    {url:'/images/users/user1.jpg', alt:'user1'},
-    {url:'/images/users/user2.jpg', alt:'user2'},
-    {url:'/images/users/user3.jpg', alt:'user3'},
-    {url:'/images/users/user4.jpg', alt:'user4'},
-    {url:'/images/users/user5.jpg', alt:'user5'},
-    {url:'/images/users/user5.jpg', alt:'user5'},
+    { url: '/images/users/user1.jpg', alt: 'user1' },
+    { url: '/images/users/user2.jpg', alt: 'user2' },
+    { url: '/images/users/user3.jpg', alt: 'user3' },
+    { url: '/images/users/user4.jpg', alt: 'user4' },
+    { url: '/images/users/user5.jpg', alt: 'user5' },
+    { url: '/images/users/user5.jpg', alt: 'user5' },
+]
+
+export const categories = [
+    { name: 'All' },
+    { name: 'Sell' },
+    { name: 'Rent' },
+
+]
+
+export const properties = [
+    {
+        url: '/images/our-news/house01.jpg',
+        category: 'Popular',
+        price: 5970,
+        title: 'Tranquil Haven in the Woods',
+        description: '103 Wright CourtBurien, WA 98168',
+        beds: 4,
+        baths: 3,
+        iconCategory: '/images/icons/icon_fire_.svg'
+    },
+
+    {
+        url: '/images/our-news/house02.jpg',
+        category: 'New Listing',
+        price: 1970,
+        title: 'Serene Retreat by the Lake',
+        description: '1964 Jehovah Drive, VA 22408',
+        beds: 3,
+        baths: 2,
+        iconCategory: '/images/icons/icon_home_.svg'
+    },
+
+    {
+        url: '/images/our-news/house03.jpg',
+        category: 'Discounted Price',
+        price: 3450,
+        title: 'Charming Cottage in the Meadow',
+        description: '1508 Centennial Farm RoadHarlan, 51537',
+        beds: 4,
+        baths: 4,
+        iconCategory: 'images/icons/icon_money.svg'
+    },
+
+    {
+        url: '/images/our-news/house04.jpg',
+        category: null,
+        price: 2389,
+        title: 'Grand Estate on the Hilltop',
+        description: '103 Wright CourtBurien, WA 98168',
+        beds: 4,
+        baths: 3,
+        iconCategory: 3
+    },
+
+
+
 ]
