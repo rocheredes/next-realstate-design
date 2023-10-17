60698384a13a4edb71d9d3bb8ab356cd82d7014c
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-10-17 09:20:06 -0400

    update companyImages

--- constant/index.js
+++ constant/index.js
@@ -1,10 +1,10 @@
 export const companyImages = [
-    {url:'/images/google.png', alt:'google'},
-    {url:'/images/amazon.png', alt:'amazon'},
-    {url:'/images/logitech.png', alt:'logitech'},
-    {url:'/images/spotify.png', alt:'spotify'},
-    {url:'/images/samsung.png', alt:'samsung'},
-    {url:'/images/netflix.png', alt:'netflix'},
+    {url:'/images/google02.png', alt:'google'},
+    {url:'/images/amazon02.png', alt:'amazon'},
+    {url:'/images/logitech02.png', alt:'logitech'},
+    {url:'/images/spotify02.png', alt:'spotify'},
+    {url:'/images/samsung02.png', alt:'samsung'},
+    {url:'/images/netflix02.png', alt:'netflix'},
 ]
 
 export const userGroupsImages = [
