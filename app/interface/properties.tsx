03e49b086f93ec3cce2476b724d2bf5c0ac3c9c5
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-10-17 23:47:02 -0400

    update properties interface

---
+++ app/interface/properties.tsx
@@ -0,0 +1,12 @@
+export interface Properties {
+    url: string
+    category?: string | null
+    price: number
+    title: string
+    description: string
+    beds: number
+    baths: number
+    iconCategory?: string | null,
+    iconColorText?: string | null,
+    iconColorBg?: string | null
+}
