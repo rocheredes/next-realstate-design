e5cdb0e1bad6f6af4e1c771f9bf4b2ab01f42e4d
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-10-22 21:41:33 -0400

    add images

---
+++ components/card/NeighborhoodProperties.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface Props {
+    imgUrl: string,
+    price: string,
+    cityName: string
+}
+
+const NeighborhoodProperties = ({
+    imgUrl,
+    price,
+    cityName
+}: Props) => {
+    return (
+        <div className='relative w-full h-40 rounded-3xl overflow-hidden'>
+            <Image
+                fill
+                src={imgUrl}
+                alt='imagen01'
+            />
+            <div
+                className='overlay items-end text-left bg-transparent bg-gradient-to-b from-black/0 to-black/100'
+            >
+                <div className=' px-2 pb-5'>
+                    <p className='text-heading3-medium font-semibold'>{price}</p>
+                    <p className='text-small-text'>{cityName}</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default NeighborhoodProperties
